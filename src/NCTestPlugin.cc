35bcf2e652222d13a63d5a672b208f329c58c55e
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-03-06 09:03:45 +0100

    Implement a test in NCTestPlugin.cc which checks that the plugin is activated

--- src/NCTestPlugin.cc
+++ src/NCTestPlugin.cc
@@ -1,3 +1,4 @@
+
 #include "NCTestPlugin.hh"
 #include "NCrystal/internal/utils/NCMsg.hh"
 #include "NCrystal/internal/utils/NCMath.hh"
@@ -15,22 +16,27 @@ void NCP::customPluginTest()
 
   NCPLUGIN_MSG("Testing plugin");
 
-  //Create some test NCMAT data. For simplicity we will here base it on an
-  //existing file, but add our @CUSTOMPLUGIN section (see NCPhysicsModel.cc for
-  //a description of the format).:
+  //Here we add a test which does not really make sense from a physics
+  //perspective, but merely test that the model is activated correctly and has
+  //an effect on cross sections when the appropriate @CUSTOM_MAGSCAT section
+  //is present in NCMAT data.
+  //
+  //To do so, we take a file (BeO) from stdlib, append a @CUSTOM_MACSCAT
+  //section, and note the effect on inelastic cross sections at 1K and 5Aa,
+  //which should make it jump from <1e-4barn/atom to around 1 barn/atom (with
+  //the given parameters).
 
   constexpr double custom_incohelas_sigma = 5.0;//barn
   constexpr double custom_incohelas_wl_threshold = 1.2;//angstrom
-  const std::string base_data = "stdlib::Al_sg225.ncmat";
+  const std::string base_data = "stdlib::BeO_sg186.ncmat";
+  const std::string cfg_params = ";temp=1K;comp=inelas";
 
   std::string testdata;
   {
     testdata = NC::FactImpl::createTextData(base_data)->rawDataCopy();
     std::ostringstream customsection;
     customsection << "@CUSTOM_" << pluginNameUpperCase() << "\n"
-                  << "  # sigma and wavelength threshold\n  "
-                  << NC::fmt(custom_incohelas_sigma) << " "
-                  << NC::fmt(custom_incohelas_wl_threshold) << "\n";
+                  << "3.66  1.5  0.4e-3  0.  1. \n";
     testdata += customsection.str();
   }
 
@@ -45,49 +51,26 @@ void NCP::customPluginTest()
   //registered after it is done running. So we simply put the raw data directly
   //into a MatCfg object (which is the C++ object representing a cfg-string):
 
-  auto cfg = NC::MatCfg::createFromRawData( std::string(testdata) );
-  auto cfg_incelasonly = NC::MatCfg::createFromRawData( std::string(testdata),
-                                                        ";comp=incoh_elas" );
-  auto scat_incelasonly = NC::createScatter( cfg_incelasonly );
+  auto cfg = NC::MatCfg::createFromRawData( std::string(testdata),
+                                            ";comp=inelas;temp=1K" );
+  auto cfg_ref = NC::MatCfg(base_data+cfg_params);
+
   auto scat = NC::createScatter( cfg );
-  auto scat_base = NC::createScatter( base_data );
-  auto scat_base_noincelas = NC::createScatter( base_data + ";incoh_elas=0" );
+  auto scat_ref = NC::createScatter( cfg_ref );
+  auto wl = NC::NeutronWavelength{ 5.0 };
 
-  for ( auto& wlval : NC::linspace(0.1,3.1,16) ) {
-    auto wl = NC::NeutronWavelength( wlval );
-    auto xs = scat.crossSectionIsotropic( wl );
-    auto xs_incelasonly = scat_incelasonly.crossSectionIsotropic( wl );
+  auto xs_ref = scat_ref.crossSectionIsotropic( wl );
+  auto xs = scat.crossSectionIsotropic( wl );
+  NCPLUGIN_MSG("Ref   BeO         XS @ "<<wl<<": "<<xs_ref
+               <<" (should be less than 1e-4barn)");
+  NCPLUGIN_MSG("Dummy BeO+MagScat XS @ "<<wl<<": "<<xs
+               <<" (should be around 1 barn)");
 
-    auto xs_base = scat_base.crossSectionIsotropic( wl );
-    auto xs_base_noincelas = scat_base_noincelas.crossSectionIsotropic( wl );
-    NCPLUGIN_MSG( "xs @ "<<wl<<" : "
-                  <<xs_base<<" (base)"<<" "
-                  <<xs<<" (new)"<<" "
-                  <<xs_incelasonly<<" (new inc elas)"
-                  );
+  nc_assert_always( xs_ref.dbl() < 1e-4 );
+  nc_assert_always( xs.dbl() > 0.8 && xs.dbl() < 1.2 );
 
-    //Test that we get the expected values:
-    double expected_xs_incelasonly = ( wlval < custom_incohelas_wl_threshold
-                                       ? custom_incohelas_sigma : 0.0 );
-    double expected_xs = expected_xs_incelasonly + xs_base_noincelas.dbl();
-    nc_assert_always( NC::floateq( expected_xs_incelasonly,
-                                   xs_incelasonly.dbl() ) );
-    nc_assert_always( NC::floateq( expected_xs, xs.dbl() ) );
-  }
-
-  //Also test the data file provided in the data/ subdirectory of our plugin:
-  auto scat_somefile
-    = NC::createScatter( "plugins::DummyPlugin/somefile.ncmat;comp=incoh_elas");
-
-  for ( auto& wlval : NC::linspace(0.1,3.1,16) ) {
-    auto wl = NC::NeutronWavelength( wlval );
-    auto xs = scat_somefile.crossSectionIsotropic( wl );
-    NCPLUGIN_MSG( "somefile.ncmat incoh-elas xs @ "<<wl<<" : "<<xs);
-    double expected_xs = ( wl.dbl() <= 4.0 ? 6.0 : 0.0 );
-    nc_assert_always( NC::floateq( expected_xs, xs.dbl() ) );
-  }
-
-  //Note: We do not test the scattering here for this simple dummy plugin.
+  //Note: We do not test the scatter sampling here, but should probably do so
+  //(to test the energy changes are as expected).
 
   NCPLUGIN_MSG("All tests of plugin were successful!");
 }
